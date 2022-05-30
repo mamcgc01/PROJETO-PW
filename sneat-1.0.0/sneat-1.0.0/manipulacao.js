e33de8e3139b06d2beeffba8cfe7c484914bb4dd
mamcgc01 <miguel.caldas99@gmail.com>
2022-05-30 17:05:46 +0100

    UPDTATE

---
+++ sneat-1.0.0/sneat-1.0.0/manipulacao.js
@@ -0,0 +1,9 @@
+const loader = require("sass-loader")
+
+document.getElementById('tabledashboard').addEventListener(loader)
+
+
+
+function buildTable(data) {
+    var tabela = document.getElementById(tabledashboard)
+}
